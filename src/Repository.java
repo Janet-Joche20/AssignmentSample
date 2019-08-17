74fc1993e5d6fda0a10404c6e4b56f24b8ac65e0
Janet-Joche20 <janet.jochebed20@gmail.com>
2019-08-17 20:51:44 +0530

    EvenNos

---
+++ src/Repository.java
@@ -0,0 +1,17 @@
+
+public class Repository {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
